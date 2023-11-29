69dacec099cbbef43e29e7972c07d0fd9d215561
Senyu Xie <1658047827@qq.com>
2023-11-29 22:59:30 +0800

    🎈 perf: 将建树的代码复杂度优化为O(n)

--- editor.py
+++ editor.py
@@ -88,10 +88,12 @@ class Editor:
             else:
                 node = Node(8, line.strip("\r\n"))
 
-            for n in reversed(node_list):
-                if n.level < node.level:
-                    n.add_child(node)
-                    break
+            idx = len(node_list) - 1
+            while idx >= 0 and node_list[idx].level >= node.level:
+                node_list.pop(idx)
+                idx -= 1
+            if len(node_list) > 0:
+                node_list[-1].add_child(node)
 
             node_list.append(node)
 
